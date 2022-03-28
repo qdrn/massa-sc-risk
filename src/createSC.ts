25b89e056c371805670ef119411a3b3c43ae436b
Adrien Laversanne-Finot <alf@massa.net>
2022-03-28 20:14:22 +0200

    Basic risk

--- src/createSC.ts
+++ src/createSC.ts
@@ -3,31 +3,50 @@
  * smartcontract.
  **/
 
-import { create_sc, include_base64, print, call, Context, Storage } from "massa-sc-std";
+import { create_sc, include_base64, print, call, unsafe_random, Context, Storage } from "massa-sc-std";
 import { JSON } from 'json-as';
+import { CellState, PlayArgs } from './helloworld'
 
 // Create SC and initialize
-function initialize(_args: string): void {
+function initialize(): string {
     const bytes = include_base64('./build/helloworld.wasm');
-    let address = create_sc(bytes);
-    print("Address = " + address);
+    let sc_address = create_sc(bytes);
+    print("Address = " + sc_address);
 
     for (let x = 0; x < 10; x++) {
         for (let y = 0; y < 10; y++) {
-            Storage.set_data_for(address, "gameState_" + x.toString() + y.toString(), "0");
+            Storage.set_data_for(sc_address, "gameState_" + x.toString() + y.toString(), "0");
         }
     }
 
-    // TODO: For testing. Remove
-    // var x = 0;
-    // var y = 0;
-    // var troops = 5;
-    // var cell_state = JSON.stringify(['sender', troops])
-    // Storage.set_data_for(address, "gameState_" + x.toString() + y.toString(), cell_state);
+    return sc_address
 }
 
 export function main(_args: string): string {
-    // print(call(addr, "initialize", "", 0));
+    const sc_address = initialize();
     print(`${JSON.parse<string[]>(Context.get_call_stack())[0]}`)
+
+    // TODO: For testing. Remove
+    // Add troops to sender
+    var x = 0;
+    var y = 0;
+    var player_address = "sender";
+    var troops = 5;
+    var cell_state = JSON.stringify<CellState>({address: player_address, n: troops});
+    var key = "gameState_" + x.toString() + y.toString();
+    Storage.set_data_for(sc_address, key, cell_state);
+    // Add troops to 2aDvGEMSQ6oL97QEExvVW9EC9dWQx8tDamgf7Ko7TM7UPirpHz
+    x = 0;
+    y = 1;
+    player_address = "2aDvGEMSQ6oL97QEExvVW9EC9dWQx8tDamgf7Ko7TM7UPirpHz";
+    troops = 3;
+    cell_state = JSON.stringify<CellState>({address: player_address, n: troops});
+    key = "gameState_" + x.toString() + y.toString();
+    Storage.set_data_for(sc_address, key, cell_state);
+    
+    print(call(sc_address, "play", JSON.stringify<PlayArgs>({x0: 0, y0: 0, x1: 0, y1: 1, n: 5}), 0));
+
+    print(Storage.get_data_for(sc_address, key));
+
     return "ok";
 }
--- src/helloworld.ts
+++ src/helloworld.ts
@@ -13,9 +13,17 @@ import { JSON } from 'json-as';
 // Address: 2SzG4Aceny8JZWurwgmEPbPBK4GeTwQBr4HY6PvGT2X4aCju6s
 
 @json
+export class CellState {
+    address: string = "";
+    n: u32 = 0;
+}
+
+@json
 export class PlayArgs {
-    x: u32 = 0;
-    y: u32 = 0;
+    x0: u32 = 0;
+    y0: u32 = 0;
+    x1: u32 = 0;
+    y1: u32 = 0;
     n: u32 = 0;
 }
 
@@ -27,8 +35,45 @@ export function play(_args: string): void {
     // Check if player owns anything in (x, y)
     
     const player_address = JSON.parse<string[]>(Context.get_call_stack())[0];
+    print(player_address);
+    
     // Throw an error if there is no one here
-    Storage.get_data("gameState_" + args.x.toString() + args.y.toString());
+    var from_cell_state = JSON.parse<CellState>(Storage.get_data("gameState_" + args.x0.toString() + args.y0.toString()));
+
+    // Update from cell state
+    const new_troops = from_cell_state.n - args.n;
+    Storage.set_data("gameState_" + args.x0.toString() + args.y0.toString(), new_troops.toString());
+    
+    // Attacker need to attack with something and less than his total troops
+    assert(args.n > 0);
+    assert(args.n <= from_cell_state.n);
+
+    var to_cell_state = Storage.get_data_or_default("gameState_" + args.x1.toString() + args.y1.toString(), "0");
+    var key = "gameState_" + args.x1.toString() + args.y1.toString();
+    if (to_cell_state == "0") {
+        to_cell_state = JSON.stringify<CellState>({address: player_address, n: args.n});
+        Storage.set_data(key, to_cell_state);
+    }
+    else {
+        const n_attacker = args.n
+        const n_defender = JSON.parse<CellState>(to_cell_state).n
+        if (n_attacker > n_defender) {
+            // Update to_cell_state
+            to_cell_state = JSON.stringify<CellState>({address: player_address, n: n_attacker - n_defender});
+            Storage.set_data(key, to_cell_state);
+        }
+        else {
+            const defender_address = JSON.parse<CellState>(to_cell_state).address;
+            to_cell_state = JSON.stringify<CellState>({address: defender_address, n: n_defender - n_attacker});
+            Storage.set_data(key, to_cell_state);
+        }
+        // Use `unsafe_random()` to put some randomness on outcome
+    }
     // Storage.set_data_for(address, "gameState_" + x.toString() + y.toString(), "0");
-    // Apply operation ? Or store and resolve later ?
+
+    // Apply operation ? Or store and resolve later ? Maybe call a async SC at a given date ?
+}
+
+function resolve(): void {
+
 }
